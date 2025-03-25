2aa9f8a56fd0a9f4561aeb6d3406cc678d5eb477
paulpls <62158353+paulpls@users.noreply.github.com>
2025-03-25 11:10:46 -0600

    Clean up texture prototype

--- src/prototypes/texture.lua
+++ src/prototypes/texture.lua
@@ -1,6 +1,6 @@
 --[[
 
-    Texture handling and caching
+    Texture prototype
 
 ]]
 
@@ -21,13 +21,13 @@ M.__name = "Texture"
 
 
 
---  Registers a texture to the textures cache.
+--  Registers a texture to the cache.
 function M.register(self, override)
     local dir = "res/texture/"
     local name = self.name
     local ext = ".png"
     local path = table.concat{ dir, name, ext }
-    local override = override and true or false
+    local override = override or false
 
     if textures[path] and not override then
         self.image = textures[path]
@@ -45,11 +45,11 @@ end
 --  Ascertains the layout of the texture image as described below in `process()`
 local function getLayout(numFaces)
     local faces = {
-        [1] = { front = 1, left = 1, back = 1, right = 1, bottom = 1, top = 1, },
-        [2] = { front = 1, left = 1, back = 1, right = 1, bottom = 2, top = 2, },
-        [3] = { front = 1, left = 1, back = 1, right = 1, bottom = 2, top = 3, },
-        [4] = { front = 1, left = 2, back = 1, right = 2, bottom = 3, top = 4, },
-        [6] = { front = 1, left = 2, back = 3, right = 4, bottom = 5, top = 6, },
+        [1] = { front = 1, left = 1, back = 1, right = 1, bottom = 1, top = 1 },
+        [2] = { front = 1, left = 1, back = 1, right = 1, bottom = 2, top = 2 },
+        [3] = { front = 1, left = 1, back = 1, right = 1, bottom = 2, top = 3 },
+        [4] = { front = 1, left = 2, back = 1, right = 2, bottom = 3, top = 4 },
+        [6] = { front = 1, left = 2, back = 3, right = 4, bottom = 5, top = 6 },
     }
 
     return faces[numFaces] or faces[1]
@@ -57,7 +57,7 @@ end
 
 
 
---  Processes the texture image and break it up into 1px columns.
+--  Processes the texture image and breaks it up into 1px columns.
 --
 --  Textures are assumed to be made up of 64x64 pixel squares,
 --  unless defined otherwise. Depending on the proportions, the
@@ -69,19 +69,19 @@ end
 --     |------------------------------------------------|
 --     | 1:1 |  1st   | All faces (used as fallback)    |
 --     |-----|--------|---------------------------------|
---     | 1:2 |  1st   | All side faces                  |
+--     | 2:1 |  1st   | All side faces                  |
 --     |     |  2nd   | Bottom and top faces            |
 --     |-----|--------|---------------------------------|
---     | 1:3 |  1st   | All side faces                  |
+--     | 3:1 |  1st   | All side faces                  |
 --     |     |  2nd   | Bottom face                     |
 --     |     |  3rd   | Top face                        |
 --     |-----|--------|---------------------------------|
---     | 1:4 |  1st   | Front and back faces            |
+--     | 4:1 |  1st   | Front and back faces            |
 --     |     |  2nd   | Left and right faces            |
 --     |     |  3rd   | Bottom face                     |
 --     |     |  4th   | Top face                        |
 --     |-----|--------|---------------------------------|
---     | 1:6 |  1st   | Front face                      |
+--     | 6:1 |  1st   | Front face                      |
 --     |     |  2nd   | Left face                       |
 --     |     |  3rd   | Back face                       |
 --     |     |  4th   | Right face                      |
