fc481ef84672d41e7e34bd1bccb2a3dbca89927e
paulpls <62158353+paulpls@users.noreply.github.com>
2025-03-25 07:56:29 -0600

    Add logic for processing textures into faces and columns

--- src/texture.lua
+++ src/texture.lua
@@ -6,6 +6,11 @@
 
 
 
+local floor = math.floor
+local gfx = love.graphics
+
+
+
 local textures = {}
 
 
@@ -17,13 +22,32 @@ M.__name = "Texture"
 
 
 --  Registers a texture to the textures cache.
-function M.register(self)
+function M.register(self, override)
     local dir = "res/texture/"
     local name = self.name
     local ext = ".png"
     local path = table.concat{ dir, name, ext }
+
+    local override = override and true or false
+    if textures[path] and not override then return end
+
     local image = gfx.newImage(path)
     textures[path] = image
+end
+
+
+
+--  Ascertains the layout of the texture image as described below in `process()`
+local function getLayout(numFaces)
+    local faces = {
+        [1] = { front = 1, left = 1, back = 1, right = 1, bottom = 1, top = 1, },
+        [2] = { front = 1, left = 1, back = 1, right = 1, bottom = 2, top = 2, },
+        [3] = { front = 1, left = 1, back = 1, right = 1, bottom = 2, top = 3, },
+        [4] = { front = 1, left = 2, back = 1, right = 2, bottom = 3, top = 4, },
+        [6] = { front = 1, left = 2, back = 3, right = 4, bottom = 5, top = 6, },
+    }
+
+    return faces[numFaces] or faces[1]
 end
 
 
@@ -37,7 +61,7 @@ end
 --     .------------------------------------------------.
 --     | W:H | Square | Usage                           |
 --     |------------------------------------------------|
---     | 1:1 |  1st   | All faces                       |
+--     | 1:1 |  1st   | All faces (used as fallback)    |
 --     |-----|--------|---------------------------------|
 --     | 1:2 |  1st   | All side faces                  |
 --     |     |  2nd   | Bottom and top faces            |
@@ -61,11 +85,34 @@ end
 --
 --  NOTE At this time, top and bottom outside faces are invisible.
 function M.process(self)
+    local image = self.image
+    local w = image:getWidth()
+    local size = 64
+    local numFaces = floor(w / size)
+    self.layout = getLayout(numFaces)
+    self.faces = {}
+
+    --  Create a list of quads (columns) for each face
+    for face = 1, numFaces do
+        local newFace = {}
+
+        --  Columns are zero-indexed because they correspond to pixel coordinates
+        for col = 0, size - 1 do
+            local x = (face - 1) * size + col
+            local y = 0
+            local quad = gfx.newQuad(x, y, size, sizw)
+            newFace[col] = quad
+        end
+
+        self.faces]face] = newFace
+    end
 end
 
 
 
 function M.load(self)
+    self:register()
+    self:process()
 end
 
 
@@ -75,11 +122,14 @@ end
 
 
 
---  Returns a quad representing a 1px wide column of a texture face.
+--  Given the name of a face, returns a quad representing a 1px-wide
+--  column of the texture. Defaults to the `front` face, column zero.
 --  NOTE Columns are zero-indexed.
-function M.getColumn(self, col, face)
+function M.getColumn(self, face, col)
+    local face = self.layout[face or "front"]
     local col = col or 0
-    local face = face or 1
+
+    return self.faces[face][col]
 end
 
 
