cffb12e5fb47312d0bcedd380448c480a07b39b3
paulpls <62158353+paulpls@users.noreply.github.com>
2025-03-25 21:47:51 -0600

    Add a block for custom functionality

---
+++ data/entity/block/custom.lua
@@ -0,0 +1,36 @@
+--[[
+
+    Custom behavior block data, for sensors and such
+
+]]
+
+
+
+local Vec2 = require "src.prototypes.vec2"
+
+
+
+local M = {}
+
+
+
+M.components = {
+    Body = {
+        position = Vec2.new(),
+    },
+    Color = {
+        normal = "red",
+    },
+    Physics = {
+        state = "static",
+        mass = 0,
+        isSensor = true,
+    },
+    Collision = {
+        classList = { "Custom" },
+    },
+}
+
+
+
+return M
